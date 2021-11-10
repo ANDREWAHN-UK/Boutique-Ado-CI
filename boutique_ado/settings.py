fd12c39a3c8ceb9f9f51d4d2fce2fc3144607688
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-11-10 10:22:17 +0000

    updated comments in bag.html and settings.py

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -70,7 +70,8 @@ TEMPLATES = [
         'OPTIONS': {
             'context_processors': [
                 'django.template.context_processors.debug',
-                'django.template.context_processors.request', #this is required by allauth, so do not touch
+                #request is required by allauth, so do not touch
+                'django.template.context_processors.request', 
                 'django.contrib.auth.context_processors.auth',
                 'django.contrib.messages.context_processors.messages',
             ],
