341a37d455fc0c5dd088cf3be2009056c1bf2248
ANDREWAHN-UK <ANDREW.AHN@HOTMAIL.CO.UK>
2021-11-18 19:34:43 +0000

    adjustments to models.py for troubleshooting; created migration

--- checkout/models.py
+++ checkout/models.py
@@ -9,12 +9,12 @@ from products.models import Product
 # Create your models here.
 
 class Order(models.Model):
-    order_number = models.CharField((max_length=32, null=False, editable=False)
-    full_name = models.CharField((max_length=50, null=False, blank=False)
-    email= models.EmailField(max_length=254, null=False, blank=False)
+    order_number = models.CharField(max_length=32, null=False, editable=False)
+    full_name = models.CharField(max_length=50, null=False, blank=False)
+    email = models.EmailField(max_length=254, null=False, blank=False)
     phone_number = models.CharField(max_length=20, null=False, blank=False)
     country = models.CharField(max_length=40, null=False, blank=False)
-    postcode = models.CharField(max_length=50, null=False, blank=True)
+    postcode = models.CharField(max_length=20, null=True, blank=True)
     town_or_city = models.CharField(max_length=40, null=False, blank=False)
     street_address1 = models.CharField(max_length=80, null=False, blank=False)
     street_address2 = models.CharField(max_length=80, null=True, blank=True)
@@ -48,11 +48,11 @@ class Order(models.Model):
         return self.order_number
 
 class OrderLineItem(models.Model):
-    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems'
-    product =models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
-    product_size = models.CharField(max_length=2, null=True, blank=True)# this refers to the sizes, e.g. XS, S...
+    order = models.ForeignKey(Order, null=False, blank=False, on_delete=models.CASCADE, related_name='lineitems')
+    product = models.ForeignKey(Product, null=False, blank=False, on_delete=models.CASCADE)
+    product_size = models.CharField(max_length=2, null=True, blank=True) # XS, S, M, L, XL
     quantity = models.IntegerField(null=False, blank=False, default=0)
-    lineitem_total = models.DecimalField(max_digits=6; decimal_places=2, null=False, blank=False, editable=False)
+    lineitem_total = models.DecimalField(max_digits=6, decimal_places=2, null=False, blank=False, editable=False)
 
     def save(self, *args, **kwargs):
         """ this sets an order number if there is none when the original save method triggers """
@@ -60,4 +60,4 @@ class OrderLineItem(models.Model):
         super().save(*args, **kwargs)
 
     def __str__(self):
-        return f'SKU {self.product.sku}' on order {self.order.order_number}
+        return f'SKU {self.product.sku} on order {self.order.order_number}'
